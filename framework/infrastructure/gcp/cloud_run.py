6f2fe8051eafe42731ae61c71b3e87771e2830d0
eshitachandwani <emchandwani@google.com>
2025-03-19 16:16:33 +0530

    policy

--- framework/infrastructure/gcp/cloud_run.py
+++ framework/infrastructure/gcp/cloud_run.py
@@ -170,6 +170,19 @@ class CloudRunApiManager(
 
 
             self.create_cloud_run_resource(self.service,service_name,service_body)
+            if is_client:
+                policy_body={}
+                policy_body={
+                    "policy": {
+                        "bindings": [
+                            {
+                                "role": "roles/run.invoker",
+                                "members": ["allUsers"]
+                            }
+                        ],
+                    },
+                }
+                self.service.projects().locations().services().setIamPolicy(resource=self.resource_full_name(service_name, "services", self.region), body=policy_body).execute() # pylint: disable=no-member
             logger.info("Deploying Cloud Run service '%s'", service_name)
             return self.get_service_uri(service_name)
 
